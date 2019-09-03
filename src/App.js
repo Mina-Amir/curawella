59830fa91f511a0e8d09ad6280011eabb488167e
Mina Amir <mina.amir25@yahoo.com>
2019-09-03 22:55:34 +0200

    add firebase and fetched data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,39 @@ import firebase from './firebase'
 import './App.css'
 
 class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      bookings: []
+    }
+  }
   componentDidMount(){
-    // let patientsRef = firebase.database().ref('/Appointments/K0jTZLvEH8WKtvWz1TGJgMXWO7y1')
-    // patientsRef.on('value', snapshot => {
-    //   let patients = snapshot.val()
-    //   console.log(patients)
-    // })
+    let patientsRef = firebase.database().ref('/Appointments/K0jTZLvEH8WKtvWz1TGJgMXWO7y1')
+    patientsRef.on('value', (snapshot) => {
+      let patients = snapshot.val()
+      let patientsKey = Object.keys(patients)
+      console.log(patients)
+      let onlineBookings1 = []
+      patientsKey.forEach(key => {
+        if(patients[key].type === "Online"){
+            if(patients[key].hasOwnProperty('bookings')){
+              let onlineReservationsKeys = Object.keys(patients[key].bookings)
+              console.log('before', onlineReservationsKeys)
+              onlineReservationsKeys.filter(reservation => ((patients[key].bookings[reservation].patName).toLowerCase()).includes("muhammad"))
+              console.log('after', onlineReservationsKeys)
+              // let onlineBookings = onlineReservationsKeys.map(reservationKey => {
+              //   patients[key].bookings[reservationKey].filter(booking => booking.)
+              //   return patients[key].bookings[reservationKey]
+              // })
+              // onlineBookings1.push(...onlineBookings)
+            }
+            
+          // onlineBookings.push(onlineBookingsKeys)
+        }
+        return null
+      })
+      console.log(onlineBookings1)  
+    })
   }
   render(){
     return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-import * as firebase from 'firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyALsrlOTYV7GBFpb8OzM_xq4yT7EG1gpBQ",
-    authDomain: "curawella-2d9a5.firebaseapp.com",
-    databaseURL: "https://curawella-2d9a5.firebaseio.com",
-    projectId: "curawella-2d9a5",
-    storageBucket: "curawella-2d9a5.appspot.com",
-    messagingSenderId: "1058698528531",
-    appId: "1:1058698528531:web:41172b2e866826d6"
-  };
+  apiKey: "AIzaSyCn8bNxiHLAAPK1Zb8Yecjp3dqP7bOXxGc",
+  authDomain: "curawella-5669a.firebaseapp.com",
+  databaseURL: "https://curawella-5669a.firebaseio.com",
+  projectId: "curawella-5669a",
+  storageBucket: "curawella-5669a.appspot.com",
+  messagingSenderId: "1019399022923",
+  appId: "1:1019399022923:web:fd21f88a77d9ec91"
+};
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  let database = firebaseConfig.database()
-  console.log(database)
+  firebase.initializeApp(firebaseConfig)
+  // let databaseRef = firebase.database().ref('/Appointments/K0jTZLvEH8WKtvWz1TGJgMXWO7y1')
+  // databaseRef.on('value', snapshot => {
+  //   console.log(snapshot.val())
+  // })
+  // console.log(databaseRef)
 
-  export default firebaseConfig
+  export default firebase
